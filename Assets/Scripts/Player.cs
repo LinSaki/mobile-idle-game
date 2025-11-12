41222712d9e5a07a9420adf00cccf294c4f4b9ad
LinSaki <saquika@sheridancollege.ca>
2025-11-12 16:41:05 -0500

    Create Pomodoro timer

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player details")]
+    private string playerName;
+    private string dateOfBirth;
+
+    [Header("Movement details")]
+    [SerializeField] float movementSpeed = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Pomodoro.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Pomodoro : MonoBehaviour
+{
+    [Header("Timer Controls")]
+    [SerializeField] private float timerStart =1500.0f;
+    [SerializeField] private float breakTimerStart = 300.0f;
+    [SerializeField] private float countdownSpeed = 1f;
+
+    [Header("Timer UI")]
+    [SerializeField] private TextMeshProUGUI timerText = null;
+    [SerializeField] private GameObject pomodorPanel;
+    [SerializeField] private GameObject breakPanel;
+    [SerializeField] private GameObject sunsetBackground;
+    [SerializeField] private GameObject starlightBackground;
+
+    private float remainingTime;
+    private const float baseInterval = 1f; // 1 second intervals
+
+    private void Start()
+    {
+        PomodoroTime();
+    }
+
+    IEnumerator CountdownCoroutine()
+    {
+        float waitTime = baseInterval / countdownSpeed;
+
+        while(remainingTime > 0)
+        {
+            yield return new WaitForSeconds(waitTime);
+            remainingTime -= baseInterval;
+            UpdateTimerText();
+        }
+        TimerFinished();
+    }
+
+    private void PomodoroTime()
+    {
+        remainingTime = timerStart;
+        UpdateTimerText();
+        StartCoroutine(CountdownCoroutine());
+    }
+
+    private void BreakTime()
+    {
+        remainingTime = breakTimerStart;
+        UpdateTimerText();
+        StartCoroutine(CountdownCoroutine());
+    }
+
+    string FormatTime(float timeLength)
+    {
+        int minutes = Mathf.FloorToInt(timeLength / 60);
+        int seconds = Mathf.FloorToInt(timeLength % 60);
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+   
+    private void UpdateTimerText()
+    {
+        timerText.text = FormatTime(remainingTime);
+    }
+
+    private void TimerFinished()
+    {
+        Debug.Log("Timer finished!");
+        StopCoroutine(CountdownCoroutine());
+        if (pomodorPanel.activeInHierarchy == true)
+        {
+            setBreakPanel();
+            BreakTime();
+        }
+        else
+        {
+            setPomodoroBackground();
+            PomodoroTime();
+        }
+    }
+
+    private void setBreakPanel()
+    {
+        pomodorPanel.SetActive(false);
+        sunsetBackground.SetActive(false);
+        breakPanel.SetActive(true);
+        starlightBackground.SetActive(true);
+    }
+
+    private void setPomodoroBackground()
+    {
+        pomodorPanel.SetActive(true);
+        sunsetBackground.SetActive(true);
+        breakPanel.SetActive(false);
+        starlightBackground.SetActive(false);
+    }
+}
